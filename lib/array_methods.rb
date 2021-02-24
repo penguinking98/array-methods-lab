e09568581f15c40f61737e65e6c484d4a14009a7
Ben Kramer <benkramer614@gmail.com>
2021-02-24 13:03:21 -0500

    commit tree 7b1095e4d8ad4587865bcf3412045a510e41bb66

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,23 +1,28 @@
 def using_include(array, element)
-
+    array.include?(element)
 end
 
 def using_sort(array)
-
+frogs = ["I","small","wow"]
+frogs.sort
 end
 
 def using_reverse(array)
-
+    frogs = ["wow","small","arrays!"]
+    frogs.reverse
 end
 
 def using_first(array)
-
+    frogs = ["wow","small","thicc"]
+    frogs.first
 end
 
 def using_last(array)
-
+    frogs = ["wow","small","arrays!",]
+    frogs.last
 end
 
 def using_size(array)
-
+    frogs = ["wow","small","arrays!","toad","mouse","cheese"]
+    frogs.size
 end
